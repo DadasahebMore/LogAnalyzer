3c988c84210bbf3e0b5a2108943ed423698a6e0e
Dadasaheb More <dadasaheb.more@hcl.com>
2022-07-25 12:57:56 +0530

    Test case updated

--- src/test/java/com/logger/LogAnalyzerApplicationTests.java
+++ src/test/java/com/logger/LogAnalyzerApplicationTests.java
@@ -1,13 +1,28 @@
 package com.logger;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.logger.util.LogEventProcessor;
 
 @SpringBootTest
 class LogAnalyzerApplicationTests {
+  private static final String FILE = "files/logfile.txt";
+  @Autowired
+  private LogEventProcessor logProcessor;
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void test_checkInputFile_correct() {
+    assertThat(logProcessor.isValidFile(FILE), is(true));
+  }
+
+
+  @Test
+  void test_processLogEvents() {
+    logProcessor.processLogEvents(FILE);
+  }
+
 
 }
